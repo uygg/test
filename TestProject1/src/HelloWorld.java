16a84a20f7b5c32e3843d1c5d8adddd53568bc1d
uygg <wdhk.1217@docomo.ne.jp>
2018-04-06 20:50:50 +0900

    test 1

---
+++ TestProject1/src/HelloWorld.java
@@ -0,0 +1,46 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// 出力
+		System.out.println("Hello World!");
+		System.out.println("こんにちは！");
+		System.out.println(123);
+		System.out.println("例外が\r\n発生しました。");
+		String yama="山山";
+		System.out.println(yama);
+		String ss2="ABC";
+		System.out.println(ss2);
+		String tel="000-000";
+		System.out.println(tel);
+		char eco='山';
+		System.out.println(eco);
+		char a1='A';
+		System.out.println(a1);
+		int nu1=12345;
+		System.out.println(nu1);
+		boolean atari=true;
+		System.out.println(atari);
+		int ae=3+3;
+		System.out.println(ae);
+		int aw=3*3;
+		System.out.println(aw);
+		String aq1="3+4";
+		System.out.println(aq1);
+		String yamada="山田";
+		System.out.println(yamada+"さん");
+		int ap=-7-3;
+		System.out.println(ap);
+		int a9=-5-4;
+		System.out.println(a9);
+		int rrt=10/3;
+		System.out.println(rrt);
+		float yrt=(float)10/3;
+		System.out.println(yrt);
+		double xrt=(double)10/3;
+		System.out.println(xrt);
+
+
+	}
+
+}
